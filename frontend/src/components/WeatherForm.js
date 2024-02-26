195425d90786225cf437d097d4dd4e1fecfe9f2f
Veeramanikandan R <119089869+Veeramanikandanr48@users.noreply.github.com>
2024-02-26 18:38:52 +0530

    Update WeatherForm.js

--- frontend/src/components/WeatherForm.js
+++ frontend/src/components/WeatherForm.js
@@ -32,7 +32,7 @@ const WeatherForm = ({ getWeatherData, loading }) => {
 
   const getCityName = async (latitude, longitude) => {
     try {
-      const apiKey = process.env.REACT_APP_GEO_API_KEY;
+      const apiKey = process.env.74c61ddec52f4b5c98e8874813b00136;
       const apiUrl = `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=${apiKey}`;
       const response = await axios.get(apiUrl);
       const { data } = response;
